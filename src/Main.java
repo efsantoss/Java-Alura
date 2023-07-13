04e41cdc627f9554274aec5170f1c67c32f083ec
efsantoss <enzo_fischer10@hotmail.com>
2023-07-13 15:21:48 -0300

    Subindo a atualização do codigo da aula 01

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,14 @@
 public class Main {
-
     public static void main(String[] args) {
-        System.out.println(" Esse eh o Screen Match");
-        System.out.println(" Filme: Top Gun: Maverick");
+        System.out.println("Esse é o Screen Match");
+        System.out.println("Filme: Top Gun: Maverick");
 
         int anoDeLancamento = 2022;
-        System.out.println("Ano de Lançamento: " + anoDeLancamento);
+        System.out.println("Ano de lançamento:" + anoDeLancamento");
         boolean incluidoNoPlano = true;
-    }
+        double notaDoFilme = 8.1;
 
+        double media = (9.8 + 6.3 + 8) / 3;
+        System.out.println(media);
+    }
 }
