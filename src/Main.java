a2c643aae637d3ffb645f2e1e748e645c4349f63
efsantoss <enzo_fischer10@hotmail.com>
2023-07-14 16:18:47 -0300

    Nessa Atualização de Código aprendi a usar a formatação de texto

--- src/Main.java
+++ src/Main.java
@@ -55,6 +55,47 @@ public class Main {
                   """;
 
 
+        /* Em Java, é possível formatar textos e números de diversas maneiras.
+        Isso pode ser útil em diversas situações, como ao exibir valores para
+        o usuário de uma maneira mais legível.
+        Uma das maneiras mais comuns de se formatar textos em Java é utilizando
+        o método format(), da classe String.
+        Esse método permite formatar um texto utilizando diversos placeholders,
+        que são representados pelo caractere % seguido de uma letra
+        que indica o tipo de dado que será inserido no placeholder.
+        Por exemplo, %s indica que uma String será inserida no placeholder, %d indica um valor inteiro
+        e %f indica um valor decimal. Vamos ver um exemplo:
+         */
+
+        String name = "Maria";
+        int idade = 30;
+        double valor = 55.9999;
+        System.out.println(String.format("Meu nome é %s, eu tenho %d anos e hoje gastei %.2f reais", name, idade, valor));
+
+        /*"Meu nome é Maria, eu tenho 30 anos e hoje gastei 55,99 reais".
+        Perceba também que o placeholder %.2f indica que o valor deve ser
+        formatado com duas casas decimais.
+         */
+
+        /*Esse exemplo do que foi feito para o String.format também pode ser usado
+        com Text Block, onde usa-se o método que citei em aula, o formatted,
+        para informar as variáveis que deverão ser utilizadas no lugar dos placeholders.
+        Veja esse exemplo:
+         */
+
+
+        String nome2 = "Joao";
+        int aulas = 4;
+
+        String mensagem2 = """
+                  Olá, %s!
+                  Boas vindas ao curso de Java.
+                  Teremos %d aulas para te mostrar o que é preciso para você dar o seu primeiro mergulho na linguagem!
+                  """.formatted(nome2, aulas);
+
+        System.out.println(mensagem);
+
+
     }
 
 }
