b464f0a3b1de6fc4fafe40cd3cc1c1855874ee4a
efsantoss <enzo_fischer10@hotmail.com>
2023-07-18 15:24:38 -0300

    Aprendendo usar ||, && e .equals junto ao if e else

--- src/Condicional.java
+++ src/Condicional.java
@@ -1,22 +1,24 @@
 public class Condicional {
-
     public static void main(String[] args) {
-
-        int anoDeLancamento = 2022;
+        int anoDeLancamento = 1990;
         boolean incluidoNoPlano = true;
         double notaDoFilme = 8.1;
         String tipoPlano = "plus";
 
+// código omitido
+
         if(anoDeLancamento >= 2022){
-            System.out.println("Lançamentos que os clientes estão curtindo!");
-        } else {
-            System.out.println("Filme retrô que vale a pena assistir!" );
+            System.out.println("Lançamento que os clientes estão curtindo");
+        }
+        else{
+            System.out.println("Filme retrô que vale apena assistir");
         }
 
-        if(incluidoNoPlano == true || tipoPlano.equals("plus"))
-            // Dois "||" significa OU (or)
-            // usar .equals() inves de == (comparar)
-
-
+        if (incluidoNoPlano == true && tipoPlano.equals("plus")){
+            // usar "||" como "OR" e .equals como "==" e "&&" como "AND"
+            System.out.println("Filme liberado");
+        } else {
+            System.out.println("Deve pagar a locação");
+        }
     }
 }
