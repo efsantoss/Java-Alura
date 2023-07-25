09f7b58de927de584ebd0c14538e6b67e18e2964
efsantoss <enzo_fischer10@hotmail.com>
2023-07-25 14:21:29 -0300

    Fazendo interação com usuario através do Scanner

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         // Esta é uma linha de comentário que será ignorada pelo compilador
@@ -61,6 +62,27 @@ public class Main {
 
             }
         }
+
+        //EXEMPLO SCANNER
+
+
+         public class ExemploScanner {
+            public static void main(String[] args) {
+                Scanner scanner = new Scanner(System.in);
+
+                System.out.print("Digite seu nome: ");
+                String nome = scanner.nextLine();
+                System.out.print("Digite sua idade: ");
+                int idade = scanner.nextInt();
+                System.out.print("Digite o valor que pretende investir esse mês: ");
+                double valor = scanner.nextDouble();
+
+                System.out.println(nome + " que tem " + idade + " anos, irá investir R$ " + valor + " esse mês.");
+
+                scanner.close();
+            }
+        }
+
 
 
     }
